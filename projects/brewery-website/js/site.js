07382704bee89b8d731ed02af90e894f21fb0759
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2018-09-08 14:26:26 +0100

    Added fallbacks and optimisations

--- projects/brewery-website/js/site.js
+++ projects/brewery-website/js/site.js
@@ -18,6 +18,9 @@ var siteNav = {
 		let mediaQuery = window.matchMedia("(max-width: 33rem)");
 		mediaQuery.addListener(siteNav.showOnWideScreens);
 		siteNav.showOnWideScreens(mediaQuery);
+
+		// Remove fallback
+		siteNav.links.classList.remove('noscript');
 	},
 
 	// Set and return whether menu is open
@@ -31,11 +34,7 @@ var siteNav = {
 		siteNav._open = boolean;
 	},
 
-	// Set and return menu text
-	get buttonText() {
-		return siteNav._buttonText.textContent;
-	},
-
+	// Set menu text
 	set buttonText(text) {
 		siteNav._buttonText.textContent = text;
 	},
@@ -104,9 +103,9 @@ var carousel = {
 	},
 
 	// Set and return spotlight beer
-	_spotlightIndex: 0,
+	_spotlight: 0,
 	get spotlight() {
-		return carousel._spotlightIndex;
+		return carousel._spotlight;
 	},
 
 	set spotlight(index) {
@@ -114,25 +113,84 @@ var carousel = {
 		if(index < 0) {
 			index = carousel.beers.length - 1;
 		}
-		carousel._spotlightIndex = index;
+		carousel._spotlight = index;
 		carousel.changeSpotlight();
 	},
 
 	setInitialSpotlight: function() {
-		let numberOfBeers = carousel.beers.length;
-		let middleBeer = Math.ceil((numberOfBeers -1) / 2);
-		carousel._spotlightIndex = middleBeer;
-		carousel.initialSpotlightIndex = middleBeer;
-		carousel.changeSpotlight();
+		// Set variables for calculations
+		carousel.beerTotalWidth = 216;
+		carousel.initialSpotlight = Math.ceil((carousel.beers.length -1) / 2);
+
+		// Change spotlight to middle beer
+		carousel.spotlight = carousel.initialSpotlight;
+
+		// Fallback - Ensure carousel is not displayed full width before checking
+		let carouselTotalWidth = carousel.beerTotalWidth * carousel.beers.length;
+		let mediaQuery = window.matchMedia("(min-width:" + carouselTotalWidth + "px)");
+
+		function checkIfNarrow(mq) {
+			if(!mq.matches) {
+				checkFallbackNeed();
+				mediaQuery.removeListener(checkIfNarrow);
+			}
+		}
+		mediaQuery.addListener(checkIfNarrow);
+		checkIfNarrow(mediaQuery);
+
+		// Check if fallback for centering carousel is necessary
+		function checkFallbackNeed() {
+			let beerProperties = carousel.beers[carousel.spotlight].getBoundingClientRect();
+			let carouselProperties = carousel.element.getBoundingClientRect();
+			let centre = (carouselProperties.width / 2) + carouselProperties.left;
+			let offset = centre - (beerProperties.width / 2);
+
+			if(beerProperties.left !== offset) {
+				window.addEventListener('resize', function() {
+					if(!carousel.fallbackRunning) {
+						carousel.fallbackRunning = true;
+						requestAnimationFrame(carousel.setOffsetFallback);
+					}
+				});
+				carousel.setOffsetFallback();
+			}
+		}
 	},
-	initialSpotlightIndex: 0,
+
+	setOffsetFallback: function() {
+		let carouselWidth = carousel.element.getBoundingClientRect().width;
+		let offset = (carouselWidth - carousel.beerTotalWidth) / 2;
+
+		carousel.offsetFallback = offset;
+		carousel.changeSpotlight();
+		carousel.fallbackRunning = false;
+	},
 
 	changeSpotlight: function() {
+		let offset = carousel.beerTotalWidth * -carousel.initialSpotlight;
+		if(carousel.offsetFallback) {
+			offset = -carousel.offsetFallback;
+		}
+		let scrollAmount = carousel.beerTotalWidth * -carousel.spotlight;
+		let translate = scrollAmount - offset;
+
 		for(let i = 0; i < carousel.beers.length; i++) {
 			let beer = carousel.beers[i];
+			let image = beer.querySelector('img');
 			let caption = beer.querySelector('figcaption');
-			let offset = carousel.spotlight - carousel.initialSpotlightIndex;
-			let translate = -14 * offset;
+
+			// Scale beers according to order
+			switch(carousel.spotlight - i) {
+				case 0:
+					image.style.transform = 'scale(1)';
+					break;
+				case 1:
+				case -1:
+					image.style.transform = 'scale(0.85)';
+					break;
+				default:
+					image.style.transform = 'scale(0.7)';
+			}
 
 			// Show text only for spotlight beer
 			if(i === carousel.spotlight) {
@@ -141,7 +199,7 @@ var carousel = {
 				caption.style.opacity = '0';
 			}
 			// Scroll spotlight beer into middle;
-			beer.style.transform = 'translateX(' + translate + 'rem)';
+			beer.style.transform = 'translateX(' + translate + 'px)';
 		}
 	}
 }
