23163fdd0b10ed7b65443416be3369cc82346448
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2019-01-27 21:54:45 +0000

    Prevented tabbing into closed menu

--- Development/js/site.js
+++ Development/js/site.js
@@ -2,18 +2,55 @@ document.addEventListener('DOMContentLoaded', initialise, {once: true});
 
 let header;
 let button;
+let menu;
 
 function initialise() {
+	// Define DOM elements
 	header = document.querySelector('#site-header');
 	button = header.querySelector('#site-nav-menu-button');
-	header.dataset.navMenuOpen = 'false';
+	menu = header.querySelector('#site-nav-menu');
+
+	// Ensure header is always shown on wide screens
+	let headerIsWide = window.matchMedia('(min-width: 36rem)');
+	headerIsWide.addListener(fixHeader);
+	fixHeader(headerIsWide);
+
+	// Enable menu button
 	button.addEventListener('click', toggleNav);
 }
 
 function toggleNav() {
-	if(header.dataset.navMenuOpen === 'true') {
-		header.dataset.navMenuOpen = 'false';
+	if(header.dataset.navMenuOpen === 'false') {
+		openMenu();
 	} else {
+		closeMenu();
+	}
+}
+
+function openMenu() {
+	menu.removeAttribute('style', 'display: none');
+	setTimeout(function() {
 		header.dataset.navMenuOpen = 'true';
+	}, 10);
+}
+
+function closeMenu() {
+	let transitionDelay = window.getComputedStyle(menu).transitionDelay;
+	let transitionDuration = window.getComputedStyle(menu).transitionDuration;
+	let transitionLength = (parseFloat(transitionDelay) + parseFloat(transitionDuration)) * 1000;
+
+	header.dataset.navMenuOpen = 'false';
+	setTimeout(function() {
+		if(header.dataset.navMenuOpen === 'false') {
+			menu.style.display = 'none';
+		}
+	}, transitionLength);
+}
+
+function fixHeader(mediaQuery) {
+	if(mediaQuery.matches) {
+		openMenu();
+	} else {
+		closeMenu();
 	}
 }
