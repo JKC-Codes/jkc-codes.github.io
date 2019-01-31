b4b299ae1b8d39f89f6b27042545ccf98acc3df6
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2019-01-31 23:55:40 +0000

    Refactored menu to use prototype rather than class for IE compatibility

--- Development/js/site.js
+++ Development/js/site.js
@@ -1,76 +1,76 @@
-class Menu {
-  constructor(stateHolder, stateController, contentHolder, mediaQuery) {
-		// Name arguments
-    this.container = stateHolder;
-    this.button = stateController;
-    this.content = contentHolder;
-		this.mediaQuery = mediaQuery;
-
-		// Start media query listener
-		this.pageIsWide = window.matchMedia('(min-width: '+ this.mediaQuery +')');
-		this.pageIsWide.addListener(this.handleViewportChange.bind(this));
-		this.handleViewportChange(this.pageIsWide);
-
-		// Listen for button clicks
-		this.button.addEventListener('click', ()=> {
-			if(this.container.classList.contains('menu-closed')) {
-				this.openMenu();
-			} else {
-				this.closeMenu();
-			}
-		});
-	}
-
-	addToDOM() {
-		this.content.removeAttribute('style', 'display: none');
-	}
-
-	removeFromDOM() {
-		this.content.style.display = 'none';
-	}
-
-	openMenu() {
-		this.addToDOM();
-		setTimeout( ()=> {
-			this.container.classList.remove('menu-closed');
-			this.container.classList.add('menu-open');
-		}, 20);
-	}
-
-	closeMenu() {
-		if(!this.pageIsWide.matches) {
-			this.container.classList.remove('menu-open');
-			this.container.classList.add('menu-closed');
-			setTimeout( ()=> {
-				if(this.container.classList.contains('menu-closed')) {
-					this.removeFromDOM();
-				}
-			}, this.transitionLength);
-		}
-	}
-
-	handleViewportChange() {
-		if(this.pageIsWide.matches) {
-			this.addToDOM();
-		} else {
-			let transitionDelay = window.getComputedStyle(this.content).transitionDelay;
-			let transitionDuration = window.getComputedStyle(this.content).transitionDuration;
-			this.transitionLength = (parseFloat(transitionDelay) + parseFloat(transitionDuration)) * 1000;
-			setTimeout( ()=> {
-				this.removeFromDOM();
-				this.closeMenu();
-			}, 20);
-		}
-	}
-}
-
 document.addEventListener('DOMContentLoaded', function() {
 	header = document.querySelector('#site-header');
 	button = header.querySelector('#site-nav-menu-button');
 	menu = header.querySelector('#site-nav-menu');
 
-	new Menu(header, button, menu, '36rem');
+	var navMenu = new Menu(header, button, menu, '36rem');
 }, {once: true});
+
+function Menu(stateHolder, stateController, contentHolder, mediaQuery) {
+	// Name arguments
+	this.container = stateHolder;
+	this.button = stateController;
+	this.content = contentHolder;
+	this.mediaQuery = mediaQuery;
+
+	// Start media query listener
+	this.pageIsWide = window.matchMedia('(min-width: '+ this.mediaQuery +')');
+	this.pageIsWide.addListener(this.handleViewportChange.bind(this));
+	this.handleViewportChange(this.pageIsWide);
+
+	// Listen for button clicks
+	this.button.addEventListener('click', function() {
+		if(this.container.classList.contains('menu-closed')) {
+			this.openMenu();
+		} else {
+			this.closeMenu();
+		}
+	}.bind(this));
+}
+
+Menu.prototype.addToDOM = function() {
+	this.content.removeAttribute('style', 'display: none');
+}
+
+Menu.prototype.removeFromDOM = function() {
+	this.content.style.display = 'none';
+}
+
+Menu.prototype.openMenu = function() {
+	this.addToDOM();
+	setTimeout(function() {
+		this.container.classList.remove('menu-closed');
+		this.container.classList.add('menu-open');
+	}.bind(this), 20);
+}
+
+Menu.prototype.closeMenu = function() {
+	if(!this.pageIsWide.matches) {
+		this.container.classList.remove('menu-open');
+		this.container.classList.add('menu-closed');
+		setTimeout(function() {
+			if(this.container.classList.contains('menu-closed')) {
+				this.removeFromDOM();
+			}
+		}.bind(this), this.transitionLength);
+	}
+}
+
+Menu.prototype.handleViewportChange = function() {
+	if(this.pageIsWide.matches) {
+		this.addToDOM();
+	} else {
+		// Set transition length before calling closeMenu
+		let transitionDelay = window.getComputedStyle(this.content).transitionDelay;
+		let transitionDuration = window.getComputedStyle(this.content).transitionDuration;
+		this.transitionLength = (parseFloat(transitionDelay) + parseFloat(transitionDuration)) * 1000;
+
+		// Ignore animation when closing menu
+		this.removeFromDOM();
+		this.closeMenu();
+	}
+}
+
 
 
 
