ea32a1383c7e5671de279bea026e02d004962592
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2019-08-14 21:16:14 +0100

    Added page load fallback to cache after 3 seconds

--- Development/serviceworker.js
+++ Development/serviceworker.js
@@ -1,49 +1,96 @@
-self.addEventListener('install', function(event) {
-	event.waitUntil(caches.open('offline')
-		.then(function(cache) {
-			return cache.addAll([
-				'/',
-				'/css/site.css',
-				'/css/home.css',
-				'/js/site.js',
-				'/img/icon-skills.svg',
-				'/img/icon-projects.svg',
-				'/img/icon-github.svg',
-				'/img/icon-linkedin.svg'
-			]);
+const cacheName = 'offline';
+const initialCache = [
+	'/',
+	'/css/site.css',
+	'/css/home.css',
+	'/js/site.js',
+	'/img/icon-skills.svg',
+	'/img/icon-projects.svg',
+	'/img/icon-github.svg',
+	'/img/icon-linkedin.svg'
+];
+const maxWaitTime = 3000;
+
+
+self.addEventListener('install', event => {
+	// Create cache
+	event.waitUntil(caches.open(cacheName)
+		.then(cache => {
+			// Add common files to the cache
+			return cache.addAll(initialCache);
 		})
 	);
 });
 
 
-self.addEventListener('activate', function(event) {
-	// Start immediately instead of waiting for a page load
+self.addEventListener('activate', event => {
+	// Start immediately instead of waiting for next page load
 	event.waitUntil(clients.claim());
 });
 
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
 	// Cache incoming requests only
   if (event.request.method === 'GET') {
 		event.respondWith(
-			// Get response from network
-			fetch(event.request)
-			.then(function(response) {
-				// Copy response as it can only be used once
-				var copyOfResponse = response.clone();
-				// Put network response in cache folder named offline
-				caches.open('offline')
-				.then(function(cache) {
-					cache.put(event.request, copyOfResponse);
+			new Promise((resolve, reject) => {
+				let loaded = false;
+				// Wait a maximum time before returning the cache
+				let timeLimit;
+				function setTimeLimit() {
+					timeLimit = setTimeout(() => {
+						if(!loaded) {
+							loaded = true;
+							resolve(caches.match(event.request));
+						}
+					}, maxWaitTime)
+				}
+
+				// Check for cached version
+				caches.match(event.request)
+				.then(cacheResponse => {
+					if(cacheResponse) {
+						// Start countdown
+						setTimeLimit();
+					}
 				})
-				// Serve response to browser
-				return response;
+
+				// Get response from network
+				fetch(event.request)
+				.then(response => {
+					// Copy response as it can only be used once
+					let copyOfResponse = response.clone();
+					// Put copy of network response in cache
+					caches.open(cacheName)
+					.then(cache => {
+						cache.put(event.request, copyOfResponse);
+					})
+					// Serve response to browser
+					if(!loaded) {
+						loaded = true;
+						clearTimeout(timeLimit);
+						resolve(response);
+					}
+				})
+				// If network response fails, try the cache
+				.catch(error => {
+					// Check for cache entry
+					caches.match(event.request)
+					.then(cacheResponse => {
+						if(cacheResponse) {
+							// Return cache entry if available
+							if(!loaded) {
+								loaded = true;
+								clearTimeout(timeLimit);
+								resolve(caches.match(event.request));
+							}
+						} else {
+							// Throw if nothing available
+							return reject(new Error(error));
+						}
+					})
+				})
 			})
-			// If network response fails
-			.catch(function(error) {
-				// Return cache entry
-				return caches.match(event.request);
-			})
-		)
+		);
 	}
 });
