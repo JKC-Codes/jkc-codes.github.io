93915cafc3047db74b9886e6c7c2262f937a7893
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-04-13 16:28:24 +0100

    Refactor site nav menu

--- Development/js/site.js
+++ Development/js/site.js
@@ -3,183 +3,152 @@ if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/serviceworker.js');
 }
 
-
-// Start javascript events
+// Create nav menu
 document.addEventListener('DOMContentLoaded', function() {
-	// Define DOM elements
+
 	var menuButton = document.querySelector('#site-nav-menu-button');
 	var menuButtonText = menuButton.querySelector('#site-nav-menu-button-text');
 	var menuContent = document.querySelector('#site-nav-content');
 
-	// Activate site nav menu
-	var siteNavMenu = new Menu(menuButton, menuButtonText, menuContent, '48rem');
+	siteNavMenu.init(menuButton, menuButtonText, menuContent, '48rem');
+
 }, {once: true});
 
+var MENU = {
+	init: function init(stateController, stateText, contentContainer, mediaQueryValue) {
+		this.button = stateController;
+		this.text = stateText;
+		this.menu = contentContainer;
+		this.minWidth = mediaQueryValue;
 
-// Menu class
-function Menu(stateController, stateControllerText, contentHolder, mediaQueryValue) {
-	// Name arguments
-	this.button = stateController;
-	this.text = stateControllerText;
-	this.container = contentHolder;
-	this.minWidth = mediaQueryValue;
+		// Create fixed references so listeners can be removed later
+		this.handleButtonClick = function handleButtonClick(event) {
+			this._handleButtonClick(event);
+		}.bind(this);
 
-	// Start viewport width listener
-	var mediaQuery = window.matchMedia('(min-width: '+ this.minWidth +')');
-	mediaQuery.addListener(this.reactToViewport.bind(this));
-	this.reactToViewport(mediaQuery);
+		this.handleClickOffMenu = function handleClickOffMenu(event) {
+			this._handleClickOffMenu(event);
+		}.bind(this);
 
-	// Start menu button click listener
-	this.button.addEventListener('click', this.reactToMenuButton.bind(this));
+		this.handleEscapeKey = function handleEscapeKey(event) {
+			this._handleEscapeKey(event);
+		}.bind(this);
 
-	// Create fixed variables so listeners can be removed later
-	this.reactToClickOffMenu = function(event) {
-		this._reactToClickOffMenu(event);
-	}.bind(this);
+		this.handleScrollAway = function handleScrollAway(event) {
+			this._handleScrollAway(event);
+		}.bind(this);
 
-	this.reactToEscapeKey = function(event) {
-		this._reactToEscapeKey(event);
-	}.bind(this);
-}
+		this.handleTransitionEnd = function handleTransitionEnd(event) {
+			this._handleTransitionEnd(event);
+		}.bind(this);
 
+		// Start viewport width listener
+		this.mediaQuery = window.matchMedia('(min-width: '+ this.minWidth +')');
+		this.mediaQuery.addListener(this.handleViewportChange.bind(this));
 
-// Menu state change handler
-Menu.prototype = {
-	get state() {
-		return this._state;
+		// Trigger menu activation
+		this.handleViewportChange(this.mediaQuery);
 	},
 
-	set state(newState) {
-		if(newState === 'open') {
-			this.updateDOM('add');
-			// Wait for menu to be added to DOM before referencing it
-			setTimeout(function() {
-				this.updateView('open');
-				this.updateButtonText('Close');
-				this.updateListeners('add');
-			}.bind(this), 50);
+	openMenu: function openMenu() {
+		// Add menu back to the DOM
+		this.menu.style.display = '';
+
+		// Force a repaint so transitions work
+		var styles = window.getComputedStyle(this.menu);
+		styles.getPropertyValue('display');
+
+		this.button.setAttribute('aria-expanded', 'true');
+		this.text.textContent = 'Close';
+
+		document.addEventListener('click', this.handleClickOffMenu, {passive: true});
+		document.addEventListener('keyup', this.handleEscapeKey, {passive: true});
+		if('IntersectionObserver' in window) {
+			this.observer = new IntersectionObserver(this.handleScrollAway, {threshold: 0.2});
+			this.observer.observe(this.menu);
+		}
+	},
+
+	closeMenu: function closeMenu() {
+		this.button.setAttribute('aria-expanded', 'false');
+		this.text.textContent = 'Open';
+
+		document.removeEventListener('click', this.handleClickOffMenu, {passive: true});
+		document.removeEventListener('keyup', this.handleEscapeKey, {passive: true});
+		if(this.observer) {
+			this.observer.unobserve(this.menu);
 		}
 
-		else if(newState === 'closed') {
-			// Prevent transition on viewport change
-			if(this.state === 'fixed') {
-				this.updateDOM('remove');
-			}
-			this.updateView('closed');
-			this.updateButtonText('Open');
-			this.updateListeners('remove');
-			// Get transition time for timeout
-			if(this.transitionLength === undefined && document.readyState === 'complete') {
+		// If mobile and not initial load
+		if(!this.mediaQuery.matches && this.menu.style.display !== 'none') {
+			if(this.transitionLength === undefined) {
 				this.getTransitionLength();
 			}
-			// Ensure menu is removed from DOM only after transition has finished
-			setTimeout(function() {
-				// Check that state hasn't changed during transition
-				if(this.state === 'closed') {
-					this.updateDOM('remove');
-				}
-			}.bind(this), this.transitionLength);
+
+			// Wait for transition end before removing from DOM
+			if(this.transitionLength > 0) {
+				this.menu.addEventListener('transitionend', this.handleTransitionEnd, {once: true});
+			}
+			else {
+				this.menu.style.display = 'none';
+			}
 		}
+	},
 
-		else if(newState === 'fixed') {
-			this.updateDOM('add');
-			this.updateListeners('remove');
+	_handleButtonClick: function _handleButtonClick() {
+		this.button.getAttribute('aria-expanded') === 'false' ? this.openMenu() : this.closeMenu();
+	},
+
+	_handleClickOffMenu: function _handleClickOffMenu() {
+		if(!event.target.closest('#site-nav-menu')) {
+			this.closeMenu();
 		}
+	},
 
-		this._state = newState;
-	}
-}
-
-
-// Menu listeners that change state
-Menu.prototype.reactToViewport = function(mediaQuery) {
-	if(mediaQuery.matches) {
-		this.state = 'fixed';
-	}
-	else {
-		this.state = 'closed';
-	}
-}
-
-Menu.prototype.reactToMenuButton = function() {
-	if(this.state === 'open') {
-		this.state = 'closed';
-	}
-	else {
-		this.state = 'open';
-	}
-}
-
-Menu.prototype._reactToClickOffMenu = function(event) {
-	if(!event.target.closest('#site-nav-content')) {
-		this.state = 'closed';
-	}
-}
-
-Menu.prototype._reactToEscapeKey = function(event) {
-	if(event.key === 'Escape' || event.key === 'Esc') {
-		this.state = 'closed';
-	}
-}
-
-Menu.prototype.reactToScrollOutOfView = function(intersection) {
-	if((intersection[0].boundingClientRect.top + window.pageYOffset) >= 0) {
-		this.state = 'closed';
-	}
-}
-
-
-// Menu handlers that react to state change handler
-Menu.prototype.updateDOM = function(action) {
-	if(action === 'add') {
-		this.container.removeAttribute('style', 'display: none');
-	}
-	else if(action === 'remove') {
-		this.container.style.display = 'none';
-	}
-}
-
-Menu.prototype.updateView = function(state) {
-	if(state === 'open') {
-		this.button.setAttribute('aria-expanded', 'true');
-	}
-	else if(state === 'closed') {
-		this.button.setAttribute('aria-expanded', 'false');
-	}
-}
-
-Menu.prototype.updateButtonText = function(text) {
-	this.text.textContent = text;
-}
-
-Menu.prototype.updateListeners = function(action) {
-	if(action === 'add') {
-		document.addEventListener('click', this.reactToClickOffMenu);
-		document.addEventListener('keyup', this.reactToEscapeKey);
-		if('IntersectionObserver' in window) {
-			this.observer = new IntersectionObserver(this.reactToScrollOutOfView.bind(this), {threshold: 0.3});
-			this.observer.observe(this.container);
+	_handleEscapeKey: function _handleEscapeKey() {
+		if(event.key === 'Escape' || event.key === 'Esc') {
+			this.closeMenu();
 		}
-	}
-	else if(action === 'remove') {
-		document.removeEventListener('click', this.reactToClickOffMenu);
-		document.removeEventListener('keyup', this.reactToEscapeKey);
-		if(this.observer) {
-			this.observer.unobserve(this.container);
+	},
+
+	_handleScrollAway: function _handleScrollAway(event) {
+		if(event[0].intersectionRatio > 0 && event[0].intersectionRatio <= 0.2) {
+			this.closeMenu();
 		}
+	},
+
+	_handleTransitionEnd: function _handleTransitionEnd() {
+		// Ensure menu is still closed
+		if(this.button.getAttribute('aria-expanded') === 'false') {
+			this.menu.style.display = 'none';
+		}
+		this.menu.removeEventListener('transitionend', this.handleTransitionEnd);
+	},
+
+	handleViewportChange: function handleViewportChange(mediaQuery) {
+		if(mediaQuery.matches) {
+			this.menu.style.display = '';
+			this.button.removeEventListener('click', this.handleButtonClick, {passive: true});
+		}
+		else {
+			this.menu.style.display = 'none';
+			this.button.addEventListener('click', this.handleButtonClick, {passive: true});
+		}
+		this.closeMenu();
+	},
+
+	getTransitionLength: function getTransitionLength() {
+		var contentStyles = window.getComputedStyle(this.menu);
+		var transitionDelay = parseFloat(contentStyles.transitionDelay);
+		var transitionDuration = parseFloat(contentStyles.transitionDuration);
+
+		this.transitionLength = (transitionDelay + transitionDuration) * 1000;
 	}
 }
-
-Menu.prototype.getTransitionLength = function() {
-	var containerStyles = window.getComputedStyle(this.container);
-	var transitionDelay = parseFloat(containerStyles.transitionDelay);
-	var transitionDuration = parseFloat(containerStyles.transitionDuration);
-
-	this.transitionLength = (transitionDelay + transitionDuration) * 1000;
-}
+var siteNavMenu = Object.create(MENU);
 
 
-// Polyfill for Element.closest
+// Polyfill for Element.closest used in clicks off menu
 if (!Element.prototype.matches) {
 	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
 }
