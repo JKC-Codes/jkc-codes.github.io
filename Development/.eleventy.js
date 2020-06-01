1294a3628728a9aa18914357f45cc83d9e2066f1
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-06-01 11:43:56 +0100

    Fix SASS not triggering browser refresh

--- Development/.eleventy.js
+++ Development/.eleventy.js
@@ -3,6 +3,9 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./js/");
 	eleventyConfig.addPassthroughCopy({"./js/serviceworker.js":"./serviceworker.js"});
 	eleventyConfig.addPassthroughCopy({"./.netlify/_redirects":"./_redirects"});
+	eleventyConfig.setBrowserSyncConfig({
+    files: "./staging/css/**/*"
+  });
   return {
     dir: {
 			input: "html/",
