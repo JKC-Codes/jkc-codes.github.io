408d79f4b0a6a511da5bbeadd028d319f09a5e89
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-07-21 16:43:27 +0100

    Add dynamic blog posts list

--- Development/.eleventy.js
+++ Development/.eleventy.js
@@ -1,24 +1,34 @@
 module.exports = function(eleventyConfig) {
 	// Move folders directly to the build directory
-	eleventyConfig.addPassthroughCopy("./img/");
-	eleventyConfig.addPassthroughCopy("./js/");
+	eleventyConfig.addPassthroughCopy('./img/');
+	eleventyConfig.addPassthroughCopy('./js/');
 
 	// Move service worker from JS folder to the root directory
-	eleventyConfig.addPassthroughCopy({"./js/serviceworker.js":"./serviceworker.js"});
+	eleventyConfig.addPassthroughCopy({'./js/serviceworker.js':'./serviceworker.js'});
 
-	// Redirect missing pages on stage to production
-	eleventyConfig.addPassthroughCopy({"./.netlify/_redirects":"./_redirects"});
+	// Redirect missing pages in stage to production
+	eleventyConfig.addPassthroughCopy({'./.netlify/_redirects':'./_redirects'});
 
 	// Refresh browser when CSS updates
 	eleventyConfig.setBrowserSyncConfig({
-	files: ["./staging/css/**/*.css", "!./staging/css/**/*.map"]
+	files: ['./staging/css/**/*.css', '!./staging/css/**/*.map']
+	});
+
+	// Create summaries for blog posts
+	eleventyConfig.addShortcode('summarise', function(article) {
+		return getSummary(article);
 	});
 
 	return {
 		dir: {
-			input: "html/",
-			output: "staging/"
+			input: 'html/',
+			output: 'staging/'
 		},
 		passthroughFileCopy: true
   };
 };
+
+function getSummary(article) {
+	let content = article.templateContent;
+	return content;
+}
