c709f807f91a3bdc969eb7d8cce149a10a0b0c32
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-08-07 15:36:05 +0100

    Publish out of docs folder rather than root

---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+const
+	gulp = require('gulp'),
+	destination = './staging/',
+	del = require('del'),
+	htmlmin = require('gulp-htmlmin'),
+	sass = require('gulp-sass'),
+	terser = require('gulp-terser'),
+	imagemin = require('gulp-imagemin'),
+	shell = require('child_process').exec
+;
+
+
+function reset() {
+	return del(`${destination}`);
+}
+
+function eleventy() {
+	return shell(`npx @11ty/eleventy --output="${destination}"`);
+}
+
+function cname() {
+	return gulp.src('./CNAME')
+		.pipe(gulp.dest(destination));
+}
+
+function html() {
+	return gulp.src(destination + '**/*.html')
+	.pipe(htmlmin({
+		collapseBooleanAttributes: true,
+		collapseInlineTagWhitespace: true,
+		collapseWhitespace: true,
+		conservativeCollapse: true,
+		minifyCSS: true,
+		minifyJS: true,
+		preserveLineBreaks: true,
+		removeComments: true,
+		removeEmptyAttributes: true,
+		removeRedundantAttributes: true,
+		removeScriptTypeAttributes: true,
+		removeStyleLinkTypeAttributes: true
+	}))
+	.pipe(gulp.dest(destination));
+}
+
+function css() {
+	return gulp.src('sass/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest(destination + 'css'));
+}
+
+function js() {
+	return gulp.src(destination + '**/*.js')
+		.pipe(terser())
+		.pipe(gulp.dest(destination));
+}
+
+function sw() {
+	return new Promise((resolve, reject) => {
+		gulp.src(destination + 'js/serviceworker.js')
+			.pipe(gulp.dest(destination))
+			.on('end', ()=> {
+				return del([destination + 'js/serviceworker.js'])
+			})
+			.on('end', resolve)
+			.on('error', reject);
+	})
+}
+
+function img() {
+	return gulp.src(destination + 'img/**')
+	.pipe(imagemin([
+		imagemin.gifsicle(),
+		imagemin.mozjpeg(),
+		imagemin.optipng(),
+		imagemin.svgo({plugins: [{removeViewBox: false}]})
+	]))
+	.pipe(gulp.dest(destination + 'img/'));
+}
+
+function netlify() {
+	return shell(`netlify deploy --dir=${destination} --prod`);
+}
+
+function browser() {
+	return shell('start firefox.exe -private-window https://jkc-codes.netlify.app');
+}
+
+function removeRedirects() {
+	return del(`${destination}_redirects`);
+}
+
+
+exports.stage = gulp.series(
+	reset,
+	eleventy,
+	gulp.parallel(
+		cname,
+		html,
+		css,
+		gulp.series(js, sw),
+		img
+	),
+	netlify,
+	browser
+);
+
+exports.publish = gulp.series(
+	reset,
+	eleventy,
+	gulp.parallel(
+		cname,
+		html,
+		css,
+		gulp.series(js, sw),
+		img,
+		removeRedirects
+	)
+);
--- js/site.js
+++ js/site.js
@@ -1,1 +1,206 @@
-function fillServiceWorkerCache(){var e=new Set;e.add(window.location.pathname),document.querySelectorAll('link[rel="stylesheet"]').forEach(function(t){e.add(t.href)}),document.querySelectorAll("script[src]").forEach(function(t){e.add(t.src)}),document.querySelectorAll("img").forEach(function(t){e.add(t.currentSrc)}),navigator.serviceWorker.controller.postMessage({command:"fillInitialCache",payload:Array.from(e)})}var MENU={init:function(e,t,n,i){this.button=e,this.text=t,this.menu=n,this.minWidth=i,this.handleButtonClick=function(e){this._handleButtonClick(e)}.bind(this),this.handleClickOffMenu=function(e){this._handleClickOffMenu(e)}.bind(this),this.handleEscapeKey=function(e){this._handleEscapeKey(e)}.bind(this),this.handleScrollAway=function(e){this._handleScrollAway(e)}.bind(this),this.handleTransitionEnd=function(e){this._handleTransitionEnd(e)}.bind(this),this.mediaQuery=window.matchMedia("(min-width: "+this.minWidth+")"),this.mediaQuery.addListener(this.handleViewportChange.bind(this)),this.button.removeAttribute("hidden"),this.handleViewportChange(this.mediaQuery)},openMenu:function(){this.menu.style.display="",window.getComputedStyle(this.menu).getPropertyValue("display"),this.button.setAttribute("aria-expanded","true"),this.text.textContent="Close",document.addEventListener("click",this.handleClickOffMenu,{passive:!0}),document.addEventListener("keyup",this.handleEscapeKey,{passive:!0}),"IntersectionObserver"in window&&(this.observer=new IntersectionObserver(this.handleScrollAway,{threshold:.2}),this.observer.observe(this.menu))},closeMenu:function(){this.button.setAttribute("aria-expanded","false"),this.text.textContent="Open",document.removeEventListener("click",this.handleClickOffMenu,{passive:!0}),document.removeEventListener("keyup",this.handleEscapeKey,{passive:!0}),this.observer&&this.observer.unobserve(this.menu),this.mediaQuery.matches||"none"===this.menu.style.display||(void 0===this.transitionLength&&this.getTransitionLength(),this.transitionLength>0?this.menu.addEventListener("transitionend",this.handleTransitionEnd,{once:!0}):this.menu.style.display="none")},_handleButtonClick:function(){"false"===this.button.getAttribute("aria-expanded")?this.openMenu():this.closeMenu()},_handleClickOffMenu:function(){event.target.closest("#site-nav-menu")||this.closeMenu()},_handleEscapeKey:function(){"Escape"!==event.key&&"Esc"!==event.key||this.closeMenu()},_handleScrollAway:function(e){e[0].intersectionRatio>0&&e[0].intersectionRatio<=.2&&this.closeMenu()},_handleTransitionEnd:function(){"false"===this.button.getAttribute("aria-expanded")&&(this.menu.style.display="none"),this.menu.removeEventListener("transitionend",this.handleTransitionEnd)},handleViewportChange:function(e){e.matches?(this.menu.style.display="",this.button.removeEventListener("click",this.handleButtonClick,{passive:!0})):(this.menu.style.display="none",this.button.addEventListener("click",this.handleButtonClick,{passive:!0})),this.closeMenu()},getTransitionLength:function(){var e=window.getComputedStyle(this.menu),t=parseFloat(e.transitionDelay),n=parseFloat(e.transitionDuration);this.transitionLength=1e3*(t+n)}};Element.prototype.matches||(Element.prototype.matches=Element.prototype.msMatchesSelector||Element.prototype.webkitMatchesSelector),Element.prototype.closest||(Element.prototype.closest=function(e){var t=this;do{if(t.matches(e))return t;t=t.parentElement||t.parentNode}while(null!==t&&1===t.nodeType);return null}),"serviceWorker"in navigator&&(navigator.serviceWorker.register("/serviceworker.js"),navigator.serviceWorker.controller||navigator.serviceWorker.addEventListener("controllerchange",fillServiceWorkerCache,{once:!0})),document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector("#site-nav-menu-button"),t=e.querySelector("#site-nav-menu-button-text"),n=document.querySelector("#site-nav-content");Object.create(MENU).init(e,t,n,"48rem")},{once:!0});
+function fillServiceWorkerCache() {
+	// Using a set prevents duplicates
+	var initialCache = new Set();
+
+	// HTML
+	initialCache.add(window.location.pathname);
+
+	// CSS
+	var stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
+	stylesheets.forEach(function(stylesheet) {
+		initialCache.add(stylesheet.href);
+	});
+
+	// JavaScript
+	var scripts = document.querySelectorAll('script[src]');
+	scripts.forEach(function(script) {
+		initialCache.add(script.src);
+	});
+
+	// Images
+	var images = document.querySelectorAll('img');
+	images.forEach(function(image) {
+		initialCache.add(image.currentSrc);
+	});
+
+	navigator.serviceWorker.controller.postMessage({
+		command: 'fillInitialCache',
+		payload: Array.from(initialCache)
+	});
+}
+
+
+// Site navigation menu
+var MENU = {
+	init: function init(stateController, stateText, contentContainer, mediaQueryValue) {
+		this.button = stateController;
+		this.text = stateText;
+		this.menu = contentContainer;
+		this.minWidth = mediaQueryValue;
+
+		// Create fixed references so listeners can be removed later
+		this.handleButtonClick = function handleButtonClick(event) {
+			this._handleButtonClick(event);
+		}.bind(this);
+
+		this.handleClickOffMenu = function handleClickOffMenu(event) {
+			this._handleClickOffMenu(event);
+		}.bind(this);
+
+		this.handleEscapeKey = function handleEscapeKey(event) {
+			this._handleEscapeKey(event);
+		}.bind(this);
+
+		this.handleScrollAway = function handleScrollAway(event) {
+			this._handleScrollAway(event);
+		}.bind(this);
+
+		this.handleTransitionEnd = function handleTransitionEnd(event) {
+			this._handleTransitionEnd(event);
+		}.bind(this);
+
+		// Start viewport width listener
+		this.mediaQuery = window.matchMedia('(min-width: '+ this.minWidth +')');
+		this.mediaQuery.addListener(this.handleViewportChange.bind(this));
+
+		// Trigger menu activation
+		this.button.removeAttribute('hidden');
+		this.handleViewportChange(this.mediaQuery);
+	},
+
+	openMenu: function openMenu() {
+		// Add menu back to the DOM
+		this.menu.style.display = '';
+
+		// Force a repaint so transitions work
+		var styles = window.getComputedStyle(this.menu);
+		styles.getPropertyValue('display');
+
+		this.button.setAttribute('aria-expanded', 'true');
+		this.text.textContent = 'Close';
+
+		document.addEventListener('click', this.handleClickOffMenu, {passive: true});
+		document.addEventListener('keyup', this.handleEscapeKey, {passive: true});
+		if('IntersectionObserver' in window) {
+			this.observer = new IntersectionObserver(this.handleScrollAway, {threshold: 0.2});
+			this.observer.observe(this.menu);
+		}
+	},
+
+	closeMenu: function closeMenu() {
+		this.button.setAttribute('aria-expanded', 'false');
+		this.text.textContent = 'Open';
+
+		document.removeEventListener('click', this.handleClickOffMenu, {passive: true});
+		document.removeEventListener('keyup', this.handleEscapeKey, {passive: true});
+		if(this.observer) {
+			this.observer.unobserve(this.menu);
+		}
+
+		// If mobile and not initial load
+		if(!this.mediaQuery.matches && this.menu.style.display !== 'none') {
+			if(this.transitionLength === undefined) {
+				this.getTransitionLength();
+			}
+
+			// Wait for transition end before removing from DOM
+			if(this.transitionLength > 0) {
+				this.menu.addEventListener('transitionend', this.handleTransitionEnd, {once: true});
+			}
+			else {
+				this.menu.style.display = 'none';
+			}
+		}
+	},
+
+	_handleButtonClick: function _handleButtonClick() {
+		this.button.getAttribute('aria-expanded') === 'false' ? this.openMenu() : this.closeMenu();
+	},
+
+	_handleClickOffMenu: function _handleClickOffMenu() {
+		if(!event.target.closest('#site-nav-menu')) {
+			this.closeMenu();
+		}
+	},
+
+	_handleEscapeKey: function _handleEscapeKey() {
+		if(event.key === 'Escape' || event.key === 'Esc') {
+			this.closeMenu();
+		}
+	},
+
+	_handleScrollAway: function _handleScrollAway(event) {
+		if(event[0].intersectionRatio > 0 && event[0].intersectionRatio <= 0.2) {
+			this.closeMenu();
+		}
+	},
+
+	_handleTransitionEnd: function _handleTransitionEnd() {
+		// Ensure menu is still closed
+		if(this.button.getAttribute('aria-expanded') === 'false') {
+			this.menu.style.display = 'none';
+		}
+		this.menu.removeEventListener('transitionend', this.handleTransitionEnd);
+	},
+
+	handleViewportChange: function handleViewportChange(mediaQuery) {
+		if(mediaQuery.matches) {
+			this.menu.style.display = '';
+			this.button.removeEventListener('click', this.handleButtonClick, {passive: true});
+		}
+		else {
+			this.menu.style.display = 'none';
+			this.button.addEventListener('click', this.handleButtonClick, {passive: true});
+		}
+		this.closeMenu();
+	},
+
+	getTransitionLength: function getTransitionLength() {
+		var contentStyles = window.getComputedStyle(this.menu);
+		var transitionDelay = parseFloat(contentStyles.transitionDelay);
+		var transitionDuration = parseFloat(contentStyles.transitionDuration);
+
+		this.transitionLength = (transitionDelay + transitionDuration) * 1000;
+	}
+}
+
+
+// Polyfill for Element.closest used in clicks off menu
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+	Element.prototype.closest = function(s) {
+		var el = this;
+
+		do {
+			if (el.matches(s)) return el;
+			el = el.parentElement || el.parentNode;
+		} while (el !== null && el.nodeType === 1);
+		return null;
+	};
+}
+
+
+if('serviceWorker' in navigator) {
+	// Start service worker
+	navigator.serviceWorker.register('/serviceworker.js');
+
+	// Cache files downloaded before service worker activated
+	if(!navigator.serviceWorker.controller) {
+		navigator.serviceWorker.addEventListener('controllerchange', fillServiceWorkerCache, {once: true})
+	}
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	// Create nav menu
+	var menuButton = document.querySelector('#site-nav-menu-button');
+	var menuButtonText = menuButton.querySelector('#site-nav-menu-button-text');
+	var menuContent = document.querySelector('#site-nav-content');
+	var siteNavMenu = Object.create(MENU);
+
+	siteNavMenu.init(menuButton, menuButtonText, menuContent, '48rem');
+
+}, {once: true});
