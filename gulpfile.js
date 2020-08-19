2d7105c1bdf8ab7c916e9c7466c1cf4670df2c9c
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-08-19 18:26:39 +0100

    Replace gulp-minifier with its dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const
 	destination = './docs/',
 	gulp = require('gulp'),
-	minify = require('gulp-minifier'),
-	imagemin = require('gulp-imagemin'),
+	minifyHTML = require('gulp-html-minimizer'),
+	minifyJS = require('gulp-terser'),
+	minifyIMG = require('gulp-imagemin'),
 	shell = require('child_process').exec
 ;
 
@@ -11,38 +12,30 @@ function reset() {
 	return shell(`npx del-cli ${destination}`);
 }
 
-function eleventy() {
-	return shell(`npx @11ty/eleventy --output="${destination}"`);
-}
-
 function cname() {
 	return gulp.src('./CNAME')
 		.pipe(gulp.dest(destination));
 }
 
-function redirect() {
-	return gulp.src('./.netlify/_redirects')
-		.pipe(gulp.dest(destination));
+function eleventy() {
+	return shell(`npx @11ty/eleventy --output="${destination}"`);
 }
 
 function html() {
 	return gulp.src(destination + '**/*.html')
-	.pipe(minify({
-		minify: true,
-		minifyHTML: {
-			collapseBooleanAttributes: true,
-			collapseInlineTagWhitespace: true,
-			collapseWhitespace: true,
-			conservativeCollapse: true,
-			minifyCSS: true,
-			minifyJS: true,
-			preserveLineBreaks: true,
-			removeComments: true,
-			removeEmptyAttributes: true,
-			removeRedundantAttributes: true,
-			removeScriptTypeAttributes: true,
-			removeStyleLinkTypeAttributes: true
-		}
+	.pipe(minifyHTML({
+		collapseBooleanAttributes: true,
+		collapseInlineTagWhitespace: true,
+		collapseWhitespace: true,
+		conservativeCollapse: true,
+		minifyCSS: true,
+		minifyJS: true,
+		preserveLineBreaks: true,
+		removeComments: true,
+		removeEmptyAttributes: true,
+		removeRedundantAttributes: true,
+		removeScriptTypeAttributes: true,
+		removeStyleLinkTypeAttributes: true
 	}))
 	.pipe(gulp.dest(destination));
 }
@@ -54,17 +47,11 @@ function css() {
 function js() {
 	return Promise.all([
 		gulp.src(['./site/js/**/*.js', '!./site/js/**/serviceworker.js'])
-			.pipe(minify({
-				minify: true,
-				minifyJS: {}
-			}))
+			.pipe(minifyJS())
 			.pipe(gulp.dest(destination + 'js/')),
 
 		gulp.src(['./site/js/**/serviceworker.js'])
-			.pipe(minify({
-				minify: true,
-				minifyJS: {}
-			}))
+			.pipe(minifyJS())
 			.pipe(gulp.dest(destination)),
 
 		gulp.src(['./site/js/**', '!./site/js/**/*.js'])
@@ -74,13 +61,18 @@ function js() {
 
 function img() {
 	return gulp.src('./site/img/**')
-	.pipe(imagemin([
-		imagemin.gifsicle(),
-		imagemin.mozjpeg(),
-		imagemin.optipng(),
-		imagemin.svgo({plugins: [{removeViewBox: false}]})
+	.pipe(minifyIMG([
+		minifyIMG.gifsicle(),
+		minifyIMG.mozjpeg(),
+		minifyIMG.optipng(),
+		minifyIMG.svgo({plugins: [{removeViewBox: false}]})
 	]))
 	.pipe(gulp.dest(destination + 'img/'));
+}
+
+function redirect() {
+	return gulp.src('./.netlify/_redirects')
+		.pipe(gulp.dest(destination));
 }
 
 function netlify() {
@@ -96,14 +88,14 @@ exports.default = gulp.series(
 	reset,
 	gulp.parallel(
 		cname,
-		redirect,
 		gulp.series(
 			eleventy,
 			html
 		),
 		css,
 		js,
-		img
+		img,
+		redirect
 	),
 	netlify,
 	gulp.parallel(
