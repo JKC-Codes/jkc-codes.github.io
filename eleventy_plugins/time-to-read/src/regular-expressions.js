c308257fedadf8564017d20e1eebbe2c77791f56
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-06 20:54:47 +0100

    Fix this binding errors

--- eleventy_plugins/time-to-read/src/regular-expressions.js
+++ eleventy_plugins/time-to-read/src/regular-expressions.js
@@ -1,25 +1,34 @@
+// Regex = '{' + 0 or more numbers + 1 or more of either 'h', 'm' or 's' + '}'
+const formatVariable = String.raw`{([0-9]*)(h+|m+|s+)}`;
+
+// Regex = 1 or more numbers
+const speedQuantity = String.raw`[0-9]+`;
+
+// Regex = 'characters' or 'words'
+const speedMeasure = String.raw`(characters|words)`;
+
+// Regex = 'hour' or 'minute' or 'second'
+const speedInterval = String.raw`(hour|minute|second)`;
+
+// Regex = quantity + space + measure + space + optional 'per ' or 'a ' or 'an ' + interval
+const speed = String.raw`^${speedQuantity} ${speedMeasure} ((per|a|an) )?${speedInterval}$`;
+
+// Regex = '<' + optional '/' + 1 or more alphanumeric characters + a non-word character + 0 or more non-'>' characters + '>'
+const htmlTags = String.raw`<\/?[a-z0-9]+\b[^>]*>`;
+
+//Regex = '<!--' + the minimal amount of 0 or more characters + '-->'
+const htmlComments = String.raw`<!--[^]*?-->`;
+
+// Regex = space character
+const htmlSpaces = String.raw`\s`;
+
 module.exports = {
-	// Regex = '{' + 0 or more numbers + 1 or more of either 'h', 'm' or 's' + '}'
-	formatVariable: String.raw`{([0-9]*)(h+|m+|s+)}`,
-
-	// Regex = 1 or more numbers
-	speedQuantity: String.raw`[0-9]+`,
-
-	// Regex = 'characters' or 'words'
-	speedMeasure: String.raw`(characters|words)`,
-
-	// Regex = 'hour' or 'minute' or 'second'
-	speedInterval: String.raw`(hour|minute|second)`,
-
-	// Regex = quantity + space + measure + space + optional 'per ' or 'a ' or 'an ' + interval
-	speed: String.raw`^${this.speedQuantity} ${this.speedMeasure} ((per|a|an) )?${this.speedInterval}$`,
-
-	// Regex = '<' + optional '/' + 1 or more alphanumeric characters + a non-word character + 0 or more non-'>' characters + '>'
-	htmlTags: String.raw`<\/?[a-z0-9]+\b[^>]*>`,
-
-	//Regex = '<!--' + the minimal amount of 0 or more characters + '-->'
-	htmlComments: String.raw`<!--[^]*?-->`,
-
-	// Regex = space character
-	htmlSpaces: String.raw`\s`,
+	formatVariable: formatVariable,
+	speedQuantity: speedQuantity,
+	speedMeasure: speedMeasure,
+	speedInterval: speedInterval,
+	speed: speed,
+	htmlTags: htmlTags,
+	htmlComments: htmlComments,
+	htmlSpaces: htmlSpaces
 }
