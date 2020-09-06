65197cb459aaaba7509ac25c87bb448c543dc3c6
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-06 20:54:26 +0100

    Prevent possible circular dependency

--- eleventy_plugins/time-to-read/src/options-parser.js
+++ eleventy_plugins/time-to-read/src/options-parser.js
@@ -1,11 +1,9 @@
 const regEx = require('./regular-expressions.js');
-const validateOptions = require('./options-validator.js');
 
 module.exports = function(customOptions) {
 	// Create object from customInstanceOptions array
 	const options = {};
 	customOptions.forEach(option => {
-		// Regex = '{' + 0 or more numbers + 1 or more 'h', 'm' or 's' + '}'
 		if(new RegExp(`${regEx.formatVariable}`,'i').test(option)) {
 			options.format = option;
 		}
@@ -13,7 +11,7 @@ module.exports = function(customOptions) {
 			options.speed = option;
 		}
 		else {
-			validateOptions(customOptions);
+			throw new Error(`Time-to-read encountered an unrecognised option: ${option}`);
 		}
 	})
 	return options;
