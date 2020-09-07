ca022ba9a19dbeec6a9b28b0f79a3f738e1a0e29
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-07 14:20:31 +0100

    Move default options into own module

--- eleventy_plugins/time-to-read/.eleventy.js
+++ eleventy_plugins/time-to-read/.eleventy.js
@@ -1,23 +1,10 @@
+const defaultOptions = require('./src/options-default.js');
 const parseOptions = require('./src/options-parser.js');
 const measureTime = require('./src/measure-time.js');
 const validateOptions = require('./src/options-validator.js');
 
 module.exports = function(eleventyConfig, customOptions) {
-
-	const defaultOptions = {
-		hour: ['h', 'hr', ' hour'],
-		hours: ['h', 'hrs', ' hours'],
-		minute: ['m', 'min', ' minute'],
-		minutes: ['m', 'mins', ' minutes'],
-		second: ['s', 'sec', ' second'],
-		seconds: ['s', 'secs', ' seconds'],
-		format: '{mmm}',
-		quantity: 1000,
-		measure: 'characters',
-		interval: 'minute'
-	};
 	const globalOptions = Object.assign({}, defaultOptions, customOptions);
-
 	eleventyConfig.addFilter('time-to-read', function(input, ...instanceOptions) {
 		try {
 			const options = Object.assign({}, globalOptions, parseOptions(instanceOptions));
---
+++ eleventy_plugins/time-to-read/src/options-default.js
@@ -0,0 +1,10 @@
+module.exports = {
+	hour: ['h', 'hr', ' hour'],
+	hours: ['h', 'hrs', ' hours'],
+	minute: ['m', 'min', ' minute'],
+	minutes: ['m', 'mins', ' minutes'],
+	second: ['s', 'sec', ' second'],
+	seconds: ['s', 'secs', ' seconds'],
+	format: '{mmm}',
+	speed: '1000 characters minute'
+}
