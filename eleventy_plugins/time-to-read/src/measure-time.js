75f9b217f888d1d1ef5b01ab23803fc9b4836486
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-07 19:47:34 +0100

    Add speed options

--- eleventy_plugins/time-to-read/src/measure-time.js
+++ eleventy_plugins/time-to-read/src/measure-time.js
@@ -1,28 +1,49 @@
 const regEx = require('./regular-expressions.js');
 
-module.exports = function(input, optionsArgument) {
-	const options = optionsArgument;
-
-	// Accept template object or a string
-	const html = input.templateContent || input;
+function convertToPlainText(content) {
+	// Convert template object to a string
+	const html = content.templateContent || content;
 
 	// Validate input
 	if(typeof html !== 'string') {
-		throw new Error("Time-to-read's input must be a string");
+		throw new Error("Time-to-read's input must be a string or template");
 	}
 
-	// Make options easily accessible
-	const speedArray = options.speed.split(' ');
-	options.speedQuantity = Number(speedArray[0]);
-	options.speedMeasure = speedArray[1];
-	options.speedInterval = speedArray[speedArray.length - 1];
-	delete options.speed;
+	// Remove html
+	return html.replace(new RegExp(regEx.html,'gi'), '');
+}
 
-	// Remove html and spaces
-	const text = html.replace(`/${regEx.html}|\s/gi`, '');
+function calculateTime(measurement, text, speed) {
+	let amount;
+	if(measurement === 'words') {
+		// Split words by whitespace and remove any empty values
+		amount = text.split(/\s/).filter(word => word);
+	}
+	else if(measurement === 'characters') {
+		// Remove all whitespace and normalise non-latin characters
+		amount = text.replace(/\s/g, '').normalize('NFC');
+	}
+	return Math.ceil(amount.length / speed);
+}
 
-	// The number of characters read per minute tends to be around 1000 for all languages: https://en.wikipedia.org/wiki/Words_per_minute#Reading_and_comprehension
-	const minutes = Math.ceil(text.length / 1000);
-	return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`;
+module.exports = function(content, options) {
+	const text = convertToPlainText(content);
+	const speedOptions = options.speed.split(' ');
+	const speedQuantity = Number(speedOptions[0]);
+	const speedMeasure = speedOptions[1];
+	const speedInterval = speedOptions[speedOptions.length - 1];
+	const time = calculateTime(speedMeasure, text, speedQuantity);
 
+	/*TODO
+		modify speed according to options:
+			upper/lower case = go over 60
+			m/mm/mmm/mmmm = narrow, short, long, custom
+			starts with 0 = only display if not 0
+			1+ = always display
+			2+ = always display with padding
+		add hour,minute,second(s)
+		replace variables with above
+	*/
+
+	return `${time} ${time === 1 ? 'minute' : 'minutes'}`
 }
