b4b09416c32580e8cfb8f7414cc978b0af85a8ea
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-07 14:46:02 +0100

    Add speed citation

--- eleventy_plugins/time-to-read/src/options-default.js
+++ eleventy_plugins/time-to-read/src/options-default.js
@@ -6,5 +6,6 @@ module.exports = {
 	second: ['s', 'sec', ' second'],
 	seconds: ['s', 'secs', ' seconds'],
 	format: '{mmm}',
+	// The number of characters read per minute tends to be around 1000 for all languages: https://en.wikipedia.org/wiki/Words_per_minute#Reading_and_comprehension
 	speed: '1000 characters minute'
 }
