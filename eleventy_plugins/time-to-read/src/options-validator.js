6fb1ec2723baf33cade114e49aee758a86b7deca
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-10 00:58:30 +0100

    Fix references to wrong variables

--- eleventy_plugins/time-to-read/src/options-validator.js
+++ eleventy_plugins/time-to-read/src/options-validator.js
@@ -2,7 +2,7 @@ const regEx = require('./regular-expressions.js');
 
 function validateSpeed(speed) {
 	if(typeof speed !== 'string') {
-		throw new Error(`Time-to-read's speed option must be a string. Received ${language}`);
+		throw new Error(`Time-to-read's speed option must be a string. Received ${speed}`);
 	}
 	else if(!new RegExp(regEx.speed,'i').test(speed)) {
 		throw new Error(`Time-to-read's speed option must be a string matching: '(Number) ${regEx.speedUnitMeasure} optional ${regEx.speedUnitPreposition} ${regEx.speedUnitInterval}'. Received '${speed}'`);
@@ -27,15 +27,15 @@ function validateLanguage(language) {
 }
 
 function validateLabel(label, optionKey) {
-	if(label === false || (optionKey.toUpperCase() === 'SECONDS' && label.toUpperCase() === 'ONLY')) {
+	if(label === false || (optionKey === 'seconds' && label.toLowerCase() === 'only')) {
 		return;
 	}
 
 	if(typeof label !== 'string') {
-		throw new Error(`Time-to-read's ${optionKey} option must be a string or False. Received ${language}`);
+		throw new Error(`Time-to-read's ${optionKey} option must be a string or False. Received ${label}`);
 	}
 
-	const secondsText = optionKey.toUpperCase() === 'SECONDS' ? " 'only', ":" ";
+	const secondsText = optionKey === 'seconds' ? " 'only', ":" ";
 	if(!new RegExp(regEx.label,'i').test(label)) {
 		throw new Error(`Time-to-read's ${optionKey} option must be a string containing${secondsText}'false' or '${regEx.labels}' with optional 'auto' separated by a space. Received '${label}'`);
 	}
@@ -52,7 +52,7 @@ function validateDigits(digits) {
 	const isInteger = Number.isInteger(digitsAsNumber);
 	const isWithinRange = digitsAsNumber >= 1 && digitsAsNumber <= 21;
 	if(!isInteger || !isWithinRange) {
-		throw new Error(`Time-to-read's digits option must be an integer from 1 to 21. Received ${insert}`);
+		throw new Error(`Time-to-read's digits option must be an integer from 1 to 21. Received ${digit}`);
 	}
 }
 
