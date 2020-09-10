9945d0008630a22eea3485fe7a0f6f38b83543f6
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-10 01:36:24 +0100

    Add label and speed parsing functions

--- eleventy_plugins/time-to-read/src/measure-time.js
+++ eleventy_plugins/time-to-read/src/measure-time.js
@@ -13,37 +13,123 @@ function convertToPlainText(content) {
 	return html.replace(new RegExp(regEx.html,'gi'), '');
 }
 
-function calculateTime(measurement, text, speed) {
-	let amount;
-	if(measurement === 'words') {
+function parseSpeedOption(speedOption) {
+	const speedOptions = speedOption.split(' ');
+	return {
+		amount: Number(speedOptions[0]),
+		measure: speedOptions[1].toLowerCase(),
+		interval: speedOptions[speedOptions.length - 1].toLowerCase()
+	};
+}
+
+function parseLabel(label) {
+	const display = new RegExp(`${regEx.labels}`,'i').exec(label);
+	return {
+		display: label === false ? false : display[0].toLowerCase(),
+		auto: new RegExp(/auto/,'i').test(label)
+	};
+}
+
+function calculateSeconds(text, { amount, measure, interval } = speedUnit) {
+	let subject;
+	if(measure === 'words') {
 		// Split words by whitespace and remove any empty values
-		amount = text.split(/\s/).filter(word => word);
+		subject = text.split(/\s/).filter(word => word);
 	}
-	else if(measurement === 'characters') {
+	else if(measure === 'characters') {
 		// Remove all whitespace and normalise non-latin characters
-		amount = text.replace(/\s/g, '').normalize('NFC');
+		subject = text.replace(/\s/g, '').normalize('NFC');
 	}
-	return Math.ceil(amount.length / speed);
+	let count = subject.length / amount;
+
+	// Normalise to seconds
+	switch(interval) {
+		case('hour'): count *= 60;
+		case('minute'): count *= 60;
+	}
+
+	return Math.ceil(count);
+}
+
+function createNumberFormat(language, unit, display, digits) {
+	return new Intl.NumberFormat(language,  {
+    style: 'unit',
+    unit: unit,
+		unitDisplay: display,
+		minimumIntegerDigits: digits
+	});
+}
+
+function constructTimeToRead(labels, seconds) {
+	if(labels.hours.display) {
+		// foo
+	}
+
+	if(labels.minutes.display) {
+		//bar
+	}
+
+	if(labels.minutes.display) {
+		//baz
+	}
+
+
+
+	// Add labels according to language and label option
+	// Add minimum digits using minimumIntegerDigits
+	// uses options' language, label, digits
+	return `${Math.ceil(seconds/60)} minutes`;
 }
 
 module.exports = function(content, options) {
 	const text = convertToPlainText(content);
-	const speedOptions = options.speed.split(' ');
-	const speedQuantity = Number(speedOptions[0]);
-	const speedMeasure = speedOptions[1];
-	const speedInterval = speedOptions[speedOptions.length - 1];
-	const time = calculateTime(speedMeasure, text, speedQuantity);
+	const speedUnits = parseSpeedOption(options.speed);
+	const labels = {
+		hours: parseLabel(options.hours),
+		minutes: parseLabel(options.minutes),
+		seconds: parseLabel(options.seconds)
+	};
+	const seconds = calculateSeconds(text, speedUnits);
+	const timeToRead = constructTimeToRead(labels, seconds);
 
-	/*TODO
-		modify speed according to options:
-			upper/lower case = go over 60
-			m/mm/mmm/mmmm = narrow, short, long, custom
-			starts with 0 = only display if not 0
-			1+ = always display
-			2+ = always display with padding
-		add hour,minute,second(s)
-		replace variables with above
-	*/
+	// {seconds: 'only'} option
+	if(options.seconds !== false && options.seconds.toLowerCase() === 'only') {
+		return seconds;
+	}
 
-	return `${time} ${time === 1 ? 'minute' : 'minutes'}`
+	let sentence = timeToRead;
+	if(options.prepend) {
+		sentence = options.prepend + sentence;
+	}
+	if(options.append) {
+		sentence = sentence + options.append;
+	}
+
+	console.log(sentence);
+	return sentence;
 }
+
+
+
+/*
+console.log(new Intl.NumberFormat("en-GB",  {
+    style: 'unit',
+    unit: "second",
+  	unitDisplay: "narrow",
+		minimumIntegerDigits: 2
+}).format(50));
+
+console.log(new Intl.NumberFormat("en-GB",  {
+    style: 'unit',
+    unit: "second",
+  	unitDisplay: "short",
+		minimumIntegerDigits: 2
+}).format(50));
+
+console.log(new Intl.NumberFormat("en-GB",  {
+    style: 'unit',
+    unit: "second",
+  	unitDisplay: "long",
+		minimumIntegerDigits: 2
+}).format(50));
+*/
