76f9b0ba3cec59a5d86b5256e494c9548c284ca0
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2021-05-03 17:27:49 +0100

    Add removal of checkbox menu fallback

--- site/Scripts/site.js
+++ site/Scripts/site.js
@@ -1,16 +1,103 @@
-document.addEventListener('DOMContentLoaded', function() {
-	// Create nav menu
-	var menuButton = document.querySelector('#site-nav-menu-button');
-	var menuButtonText = menuButton.querySelector('#site-nav-menu-button-text');
-	var menuContent = document.querySelector('#site-nav-content');
-	var siteNavMenu = Object.create(MENU);
-
-	siteNavMenu.init(menuButton, menuButtonText, menuContent, '55em');
-
-}, {once: true});
-
+document.addEventListener('DOMContentLoaded', activateSiteNavMenu, {once: true});
 
 if('serviceWorker' in navigator) {
+	activateServiceWorker();
+}
+
+
+function activateSiteNavMenu() {
+	var input = document.querySelector('.js-site-nav-input');
+	var label = document.querySelector('.js-site-nav-button');
+
+	// Remove checkbox and replace label with a button
+	removeFallback();
+
+	var button = document.querySelector('.js-site-nav-button');
+	var buttonText = button.querySelector('.js-site-nav-button-text');
+	var menu = document.querySelector('.js-site-nav-menu');
+	var mediaQuery = window.matchMedia('(min-width: 55em)');
+
+	button.addEventListener('click', toggleNavMenu, {passive: true});
+
+	mediaQuery.addEventListener('change', handleViewportChange);
+	handleViewportChange();
+
+	// If checkbox was checked before it was replaced, add listeners
+	if(button.getAttribute('aria-expanded') === 'true') {
+		openNavMenu();
+	}
+
+
+	function removeFallback() {
+		var menuIsOpen = input.checked;
+		var children = label.childNodes;
+		var button = document.createElement('button');
+
+		input.checked = false; // Fix Firefox persisting open menu on refresh
+		input.parentNode.removeChild(input);
+
+		button.classList.add('menu-button');
+		button.classList.add('js-site-nav-button');
+		button.setAttribute('type', 'button');
+		button.setAttribute('aria-controls', 'site-nav-menu');
+		button.setAttribute('aria-expanded', menuIsOpen.toString());
+		while(children.length > 0) {
+			button.appendChild(children[0]);
+		}
+
+		label.parentNode.replaceChild(button, label);
+	}
+
+	function toggleNavMenu() {
+		button.getAttribute('aria-expanded') === 'true' ? closeNavMenu() : openNavMenu();
+	}
+
+	function openNavMenu() {
+		button.setAttribute('aria-expanded', 'true');
+		buttonText.textContent = 'Close menu';
+		document.addEventListener('keydown', handleKeyDown, {passive: true});
+		document.addEventListener('click', handlePageClick, {passive: true});
+	}
+
+	function closeNavMenu() {
+		button.setAttribute('aria-expanded', 'false');
+		buttonText.textContent = 'Open menu';
+		document.removeEventListener('keydown', handleKeyDown, {passive: true});
+		document.removeEventListener('click', handlePageClick, {passive: true});
+	}
+
+	function handleViewportChange() {
+		// If nav menu is hidden remove listeners
+		if(mediaQuery.matches) {
+			closeNavMenu();
+		}
+		else {
+			// Prevent transition when header becomes menu
+			menu.classList.add('js-prevent-transition');
+			// Force a repaint while transitions disabled
+			window.getComputedStyle(menu).getPropertyValue('transition-duration');
+			menu.classList.remove('js-prevent-transition');
+		}
+	}
+
+	function handleKeyDown(event) {
+		if(event.key === 'Escape' || event.key === 'Esc') {
+			closeNavMenu();
+		}
+	}
+
+	function handlePageClick(event) {
+		var isButton = event.target.closest('.js-site-nav-button');
+		var isMenu = event.target.closest('.js-site-nav-menu');
+
+		// Close menu if there's a click outside of it
+		if(!isButton && !isMenu) {
+			closeNavMenu();
+		}
+	}
+}
+
+function activateServiceWorker() {
 	// Start service worker
 	navigator.serviceWorker.register('/serviceworker.js');
 
@@ -18,176 +105,42 @@ if('serviceWorker' in navigator) {
 	if(!navigator.serviceWorker.controller) {
 		navigator.serviceWorker.addEventListener('controllerchange', fillServiceWorkerCache, {once: true})
 	}
-}
 
-function fillServiceWorkerCache() {
-	// Using a set prevents duplicates
-	var initialCache = new Set();
+	function fillServiceWorkerCache() {
+		// Stop duplicates causing service worker cache.put errors
+		var initialCache = new Set();
 
-	// HTML
-	initialCache.add(window.location.pathname);
+		// HTML
+		initialCache.add(window.location.pathname);
 
-	// CSS
-	var stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
-	stylesheets.forEach(function(stylesheet) {
-		initialCache.add(stylesheet.href);
-	});
+		// CSS
+		var stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
+		stylesheets.forEach(function(stylesheet) {
+			initialCache.add(stylesheet.href);
+		});
 
-	// JavaScript
-	var scripts = document.querySelectorAll('script[src]');
-	scripts.forEach(function(script) {
-		initialCache.add(script.src);
-	});
+		// JavaScript
+		var scripts = document.querySelectorAll('script[src]');
+		scripts.forEach(function(script) {
+			initialCache.add(script.src);
+		});
 
-	// Images
-	var images = document.querySelectorAll('img');
-	images.forEach(function(image) {
-		initialCache.add(image.currentSrc);
-	});
+		// Images
+		var images = document.querySelectorAll('img');
+		images.forEach(function(image) {
+			initialCache.add(image.currentSrc);
+		});
 
-	navigator.serviceWorker.controller.postMessage({
-		command: 'fillInitialCache',
-		payload: Array.from(initialCache)
-	});
-}
-
-
-// Site navigation menu
-var MENU = {
-	init: function init(stateController, stateText, contentContainer, mediaQueryValue) {
-		this.button = stateController;
-		this.text = stateText;
-		this.menu = contentContainer;
-		this.minWidth = mediaQueryValue;
-
-		// Create fixed references so listeners can be removed later
-		this.handleButtonClick = function handleButtonClick(event) {
-			this._handleButtonClick(event);
-		}.bind(this);
-
-		this.handleClickOffMenu = function handleClickOffMenu(event) {
-			this._handleClickOffMenu(event);
-		}.bind(this);
-
-		this.handleEscapeKey = function handleEscapeKey(event) {
-			this._handleEscapeKey(event);
-		}.bind(this);
-
-		this.handleScrollAway = function handleScrollAway(event) {
-			this._handleScrollAway(event);
-		}.bind(this);
-
-		this.handleTransitionEnd = function handleTransitionEnd(event) {
-			this._handleTransitionEnd(event);
-		}.bind(this);
-
-		// Start viewport width listener
-		this.mediaQuery = window.matchMedia('(min-width: '+ this.minWidth +')');
-		this.mediaQuery.addListener(this.handleViewportChange.bind(this));
-
-		// Trigger menu activation
-		this.button.removeAttribute('hidden');
-		this.handleViewportChange(this.mediaQuery);
-	},
-
-	openMenu: function openMenu() {
-		// Add menu back to the DOM
-		this.menu.style.display = '';
-
-		// Force a repaint so transitions work
-		var styles = window.getComputedStyle(this.menu);
-		styles.getPropertyValue('display');
-
-		this.button.setAttribute('aria-expanded', 'true');
-		this.text.textContent = 'Close';
-
-		document.addEventListener('click', this.handleClickOffMenu, {passive: true});
-		document.addEventListener('keyup', this.handleEscapeKey, {passive: true});
-		if('IntersectionObserver' in window) {
-			this.observer = new IntersectionObserver(this.handleScrollAway, {threshold: 0.2});
-			this.observer.observe(this.menu);
-		}
-	},
-
-	closeMenu: function closeMenu() {
-		this.button.setAttribute('aria-expanded', 'false');
-		this.text.textContent = 'Open';
-
-		document.removeEventListener('click', this.handleClickOffMenu, {passive: true});
-		document.removeEventListener('keyup', this.handleEscapeKey, {passive: true});
-		if(this.observer) {
-			this.observer.unobserve(this.menu);
-		}
-
-		// If mobile and not initial load
-		if(!this.mediaQuery.matches && this.menu.style.display !== 'none') {
-			if(this.transitionLength === undefined) {
-				this.getTransitionLength();
-			}
-
-			// Wait for transition end before removing from DOM
-			if(this.transitionLength > 0) {
-				this.menu.addEventListener('transitionend', this.handleTransitionEnd, {once: true});
-			}
-			else {
-				this.menu.style.display = 'none';
-			}
-		}
-	},
-
-	_handleButtonClick: function _handleButtonClick() {
-		this.button.getAttribute('aria-expanded') === 'false' ? this.openMenu() : this.closeMenu();
-	},
-
-	_handleClickOffMenu: function _handleClickOffMenu() {
-		if(!event.target.closest('#site-nav-menu')) {
-			this.closeMenu();
-		}
-	},
-
-	_handleEscapeKey: function _handleEscapeKey() {
-		if(event.key === 'Escape' || event.key === 'Esc') {
-			this.closeMenu();
-		}
-	},
-
-	_handleScrollAway: function _handleScrollAway(event) {
-		if(event[0].intersectionRatio > 0 && event[0].intersectionRatio <= 0.2) {
-			this.closeMenu();
-		}
-	},
-
-	_handleTransitionEnd: function _handleTransitionEnd() {
-		// Ensure menu is still closed
-		if(this.button.getAttribute('aria-expanded') === 'false') {
-			this.menu.style.display = 'none';
-		}
-		this.menu.removeEventListener('transitionend', this.handleTransitionEnd);
-	},
-
-	handleViewportChange: function handleViewportChange(mediaQuery) {
-		if(mediaQuery.matches) {
-			this.menu.style.display = '';
-			this.button.removeEventListener('click', this.handleButtonClick, {passive: true});
-		}
-		else {
-			this.menu.style.display = 'none';
-			this.button.addEventListener('click', this.handleButtonClick, {passive: true});
-		}
-		this.closeMenu();
-	},
-
-	getTransitionLength: function getTransitionLength() {
-		var contentStyles = window.getComputedStyle(this.menu);
-		var transitionDelay = parseFloat(contentStyles.transitionDelay);
-		var transitionDuration = parseFloat(contentStyles.transitionDuration);
-
-		this.transitionLength = (transitionDelay + transitionDuration) * 1000;
+		// Send files to the service worker to cache
+		navigator.serviceWorker.controller.postMessage({
+			command: 'fillInitialCache',
+			payload: Array.from(initialCache)
+		});
 	}
 }
 
 
-// Polyfill for Element.closest used in clicks off menu
+// Polyfill for Element.closest
 if (!Element.prototype.matches) {
 	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
 }
