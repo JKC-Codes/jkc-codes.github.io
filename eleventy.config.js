cf01c78980a4410d623abd3684e96e06dff539f0
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2023-01-28 21:35:53 +0000

    Fix transforms not running

--- eleventy.config.js
+++ eleventy.config.js
@@ -58,7 +58,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(markdownTrimTrailingNewline);
 
 	eleventyConfig.addTransform('posthtml', function(HTMLString) {
-		if(this.page.outputPath.outputFileExtension === 'html') {
+		if(this.page.outputFileExtension === 'html') {
 			return posthtml([
 				pluginAutomaticNoopener(optionsAutomaticNoopener),
 				pluginCodeStyleHooks(optionsCodeStyleHooks),
